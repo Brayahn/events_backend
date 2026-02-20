b38df4d9ec3087dd78272b4894cab275aae870b2
Brian Maina <brayahnmainah@gmail.com>
2026-02-20 23:42:39 +0300

    deploy script to factor Base URL

--- redeploy.sh
+++ redeploy.sh
@@ -3,7 +3,6 @@
 echo "Stopping and removing old container..."
 docker stop events_webhook_server 2>/dev/null
 docker rm events_webhook_server 2>/dev/null
-docker rmi events_webhook_server 2>/dev/null
 
 echo "Rebuilding Docker image..."
 docker build -t events_webhook_server .
@@ -14,6 +13,7 @@ docker run -d \
   --restart unless-stopped \
   -p 4455:4455 \
   -e PORT=4455 \
+  -e BASE_URL='https://forms.monday.com/forms/' \
   -e MONDAY_API_KEY='eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjMzMzg0NDUzNiwiYWFpIjoxMSwidWlkIjo1NzI1NDM4OSwiaWFkIjoiMjAyNC0wMy0xNlQxOTo1MTo1My4wMDBaIiwicGVyIjoibWU6d3JpdGUiLCJhY3RpZCI6MTQ5Mjc2NzgsInJnbiI6InVzZTEifQ.GzG-PARLDqJnQBQkff9Nj95pWdbc9CTRziyF4QdFNH4' \
   --log-opt max-size=10m \
   --log-opt max-file=3 \
@@ -22,7 +22,3 @@ docker run -d \
 echo "Container started!"
 echo "Viewing logs (Ctrl+C to exit):"
 docker logs -f events_webhook_server
-
-
-
-
